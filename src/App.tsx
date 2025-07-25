efdd949281c7b4c3e1f72839097193c780a67cfc
djihad <hamdi.djihad02@gmail.com>
2025-07-25 12:21:10 +0100

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button, HStack } from "@chakra-ui/react"
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
+  const isAboveLg = useBreakpointValue({ base: false, lg: true });
 
   return (
     <>
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        {isAboveLg && (
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        )}
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
